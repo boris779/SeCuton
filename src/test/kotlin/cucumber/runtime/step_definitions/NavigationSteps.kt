bed8b6bc5b66724531aa20abbdcf9c3383072796
Boris Wrubel <boris.wrubel@extern.wrwks.at>
2019-03-21 20:55:18 +0100

    removed german feature file added navigationseteps

--- src/test/kotlin/cucumber/runtime/step_definitions/NavigationSteps.kt
+++ src/test/kotlin/cucumber/runtime/step_definitions/NavigationSteps.kt
@@ -2,10 +2,12 @@ package cucumber.runtime.step_definitions
 
 import cucumber.api.Scenario
 import cucumber.api.java.Before
-import cucumber.api.java.en.Then
+import cucumber.api.java.de.Angenommen
+import cucumber.api.java.de.Wenn
+import cucumber.api.java.en.Given
 
 
-class MainPageSteps() : AbstractStepDefs() {
+class NavigationSteps : AbstractStepDefs() {
 
     @Before
     fun beforeScrenario(scenario: Scenario) {
@@ -14,9 +16,14 @@ class MainPageSteps() : AbstractStepDefs() {
         current_scenario = scenario;
     }
 
+    @Angenommen("die Startseite ist geladen")
+    @Wenn("die Startseite geladen ist")
+    fun dieStartseiteGeladenIst() {
+        //FIXME
+    }
 
-    @Then("the peso-logo should be displayed")
-    fun the_pesologo_should_be_displayed() {
+    @Given("the Startpage is loaded")
+    fun the_Startpage_is_loaded() {
         // Write code here that turns the phrase above into concrete actions
         throw cucumber.api.PendingException();
     }
