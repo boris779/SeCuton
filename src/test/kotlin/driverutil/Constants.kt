f350d2c48d8df903c1e6ff5dabc8807005534115
Boris Wrubel <boris.wrubel@extern.wrwks.at>
2019-03-21 18:21:34 +0100

    webdriver constances

---
+++ src/test/kotlin/driverutil/Constants.kt
@@ -0,0 +1,23 @@
+package driverutil
+
+enum class DriverType {
+    CHROME,
+    FIREFOX,
+    OPERA,
+    EDGE,
+    IE,
+    REMOTE_CHROME,
+    REMOTE_FIREFOX,
+    REMOTE_ANDROID,
+    ANDROID_DEVICE,
+}
+
+enum class ScreenResolutions (val resolution : String, val  width: Int, val height: Int) {
+    desktop_1920("1920x1080x24", 1920, 1080),
+    desktop_1440("1440x900x24",1440,900),
+    desktop_1024("1024x768x24",1024,768),
+    tablet_ls_768("768x576x24",768,576),
+    tablet_p_768("576x768x24", 576, 768),
+    phone_ls_320("620x320x24", 620, 320),
+    phone_p_320("320x620x24", 320, 620)
+}
