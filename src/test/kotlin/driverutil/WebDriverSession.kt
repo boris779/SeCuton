9d2113525f0d3c5216a995ac5bf58d15981d4cc0
Boris Wrubel <boris.wrubel@gmx.at>
2019-06-20 00:35:01 +0200

    refactoring of session and pagehandling

--- src/test/kotlin/driverutil/WebDriverSession.kt
+++ src/test/kotlin/driverutil/WebDriverSession.kt
@@ -5,6 +5,8 @@ import cucumber.api.Scenario
 import org.openqa.selenium.WebDriver
 import pageobjects.AbstractPage
 import pageobjects.PageUrls
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 class WebDriverSession(val testId: String) {
 
@@ -23,27 +25,40 @@ class WebDriverSession(val testId: String) {
         currentPage = null
     }
 
-    //TODO Change from Class to KClass @Andreas
-    fun setCurrentPageClass(currentPageClass: Class<out AbstractPage>) {
-        lastPage = currentPage
-        currentPage = currentPageClass.getDeclaredConstructor(WebDriver::class.java).newInstance(webDriver)
-    }
-
-    fun setCurrentPageFromPage(currentPage: AbstractPage) {
-        setCurrentPageClass(currentPage.javaClass)
-    }
-
     private fun getDomainFromBaseUrl(): String {
         return "^http[s]?:\\/\\/([^:\\/]*)".toRegex().find(baseUrl)!!.groups.get(1)!!.value
     }
 
 
-    fun gotoUrl(subUrl: PageUrls, pageClass: Class<out AbstractPage>, scenario: Scenario) {
+    fun gotoUrl(subUrl: PageUrls, pageClass: KClass<out AbstractPage>, scenario: Scenario) {
 
         val fullUrl = baseUrl + subUrl.subUrl
         webDriver.get(fullUrl)
         scenario.write("URL used: " + fullUrl)
-        setCurrentPageClass(pageClass)
+        openPage(subUrl, pageClass)
+    }
+
+    fun <T : AbstractPage> openPage(subUrl: PageUrls, pageClass: KClass<T>): T {
+
+        val fullUrl = baseUrl + subUrl.subUrl
+        webDriver.get(fullUrl)
+        return pageOpened(pageClass)
+    }
+
+    private fun <T : AbstractPage> pageOpened(currentPageClass: KClass<T>): T {
+        val primaryConstructor = currentPageClass.primaryConstructor
+        if (primaryConstructor == null || primaryConstructor.parameters.size != 1 || primaryConstructor.parameters[0].type.classifier != WebDriverSession::class) {
+            fail("invalid primary constructor of page object")
+        }
+        return primaryConstructor!!.call(this)
+
+    }
+
+    fun activate(page: AbstractPage) {
+        if (lastPage != null) {
+            lastPage = currentPage
+        }
+        currentPage = page
     }
 
 }
