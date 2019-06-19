fdd95afc845b8d5ef34f120bf000159e687dba53
Boris Wrubel <boris.wrubel@gmx.at>
2019-06-20 00:34:36 +0200

    changed constructor for pages

--- src/test/kotlin/pageobjects/MainPage.kt
+++ src/test/kotlin/pageobjects/MainPage.kt
@@ -2,11 +2,12 @@ package pageobjects
 
 import assertk.assert
 import assertk.assertions.isEqualTo
+import driverutil.WebDriverSession
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import java.net.URLDecoder
 
-class MainPage(driver: WebDriver): AbstractPage(driver!!) {
+class MainPage(session: WebDriverSession): AbstractPage(session) {
 
 
 
