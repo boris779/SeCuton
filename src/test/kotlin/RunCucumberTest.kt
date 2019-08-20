33e8ca9086705635ae03902ecdc484febe00fcd8
Boris Wrubel <boris.wrubel@extern.wrwks.at>
2019-08-20 10:28:41 +0200

    Update, because cucumber has changed package names

--- src/test/kotlin/RunCucumberTest.kt
+++ src/test/kotlin/RunCucumberTest.kt
@@ -1,5 +1,5 @@
-import cucumber.api.CucumberOptions
-import cucumber.api.junit.Cucumber
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
