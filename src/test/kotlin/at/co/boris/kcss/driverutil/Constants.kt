76628c548b6657f60ba1eebc3ad2c6691a1db359
Boris Wrubel <boris.wrubel@extern.wrwks.at>
2019-08-27 00:11:43 +0200

    removed old screen resolutions

--- src/test/kotlin/at/co/boris/kcss/driverutil/Constants.kt
+++ src/test/kotlin/at/co/boris/kcss/driverutil/Constants.kt
@@ -18,17 +18,6 @@ enum class DriverType {
     CHROME_MOBILE_EMULATION
 }
 
-enum class ScreenResolutions (val resolution : String, val  width: Int, val height: Int) {
-    desktop_1920("1920x1080x24", 1920, 1080),
-    desktop_1440("1440x900x24",1440,900),
-    desktop_1024("1024x768x24",1024,768),
-    tablet_ls_768("768x576x24",768,576),
-    tablet_p_768("576x768x24", 576, 768),
-    phone_ls_320("620x320x24", 620, 320),
-    phone_p_320("320x620x24", 320, 620)
-}
-
-
 enum class ScreenDimension (val dimension: Dimension) {
     Desktop_1080 (Dimension(1920, 1080)),
     Desktop_720  (Dimension(1280,720)),
